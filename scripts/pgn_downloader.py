b39e1a04315e0fb151ae13d9c8331974aa14b0f5
Luc Doh <luc.dhauthuille@gmail.com>
2021-03-28 19:15:09 -0700

    PGNs downloaded into organized directories

--- scripts/pgn_downloader.py
+++ scripts/pgn_downloader.py
@@ -1,6 +1,8 @@
 import sys
 import urllib.request
 import json 
+from pathlib import Path
+
 
 class GameDownloader:
     ''' Download all games played on a chess.com for a user.
@@ -25,30 +27,53 @@ class GameDownloader:
     def get_games(self, limit = None, specify_dates = None):
         # For a year/month: https://api.chess.com/pub/player/{user}/games/2009/10/pgn
         # Games are separated by blank lines
-        pgns = []
+        pgns = {}
         dates = specify_dates if specify_dates else self.dates[:limit]
         for year, month in dates:
             games_url = f'https://api.chess.com/pub/player/{self.username}/games/{year}/{month}/pgn'
             with urllib.request.urlopen(games_url) as url:
                 content = url.read().decode()
-                pgns.extend(content.split('\n\n\n')) # Games are separated by two blank lines
+                pgns[(year, month)] = content.split('\n\n\n') # Dictionary indexed by (year, month)
 
-        print(f"Got {len(pgns)} games!")
+        print(f"Got {sum([len(games_my) for games_my in pgns.values()])} games!")
         self.games = pgns
     
     def games_to_file(self, fname):
-        with open(fname, 'w') as f:
-            for game in self.games:
-                f.write(game + '\n\n\n')
+        # Stores all games into one long text file, split by two blank lines
+        with open(fname, 'w') as f: 
+            #for game in self.games:
+            for k, games in self.games.items():
+                for game in games:
+                    f.write(game + '\n\n\n')
         print(f"Wrote to {fname}")
-            
+
+    def games_to_folder(self, foldername = None):
+        # Store all games into a folder, subdirectories by year and month
+
+        # Make base folder
+        foldername = foldername if foldername else self.username
+        full_path = f"../data/user_games/{foldername}"
+        Path(full_path).mkdir(parents=True, exist_ok=True)
+        print(f"Writing to {full_path}")
+
+        for d in self.dates:
+            # Make .../year/month subdirectory
+            subdirectory_path = f"../data/user_games/{foldername}/{d[0]}/{d[1]}/"
+            Path(subdirectory_path).mkdir(parents=True, exist_ok=True)
+            for i, g in enumerate(self.games[tuple(d)]):
+                gamename = f"{subdirectory_path}game_{i}.txt"
+                with open(gamename, 'w') as f:
+                    f.write(g)
+
+
 def main():
     if len(sys.argv) > 1:
         username = sys.argv[1]
         game_downloader = GameDownloader(username)
         game_downloader.get_dates()
-        pgns = game_downloader.get_games() 
-        game_downloader.games_to_file(f"../data/all_{username}_games.txt")
+        game_downloader.get_games() 
+        game_downloader.games_to_folder()
+
     else:
         print("No username provided...")
 
